0239287be74f0f48582d3e0b73605587b3b81833
Paul Edwards <paul.edwards@microsoft.com>
2020-10-07 17:44:58 +0100

    Added material UI

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import logo from './../../logo.svg';
 import './App.css';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import { Button, Paper } from '@material-ui/core';
 
 function App() {
 
@@ -13,25 +16,23 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
-      <header className="App-header">
+    <Container maxWidth="sm" className="App">
+      <Paper>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-
-        <p>The current time is {currentTime}.</p>
-      </header>
-    </div>
+        <Typography variant="h4" component="h2" gutterBottom>
+          Create React App + Flask + Material-UI
+        </Typography>
+        <Typography variant="subtitle1" component="h2" gutterBottom>
+          The current time is {currentTime}.
+        </Typography>
+        <Button variant="contained" color="primary">
+          Primary Button
+        </Button>
+        <Button variant="contained" color="secondary">
+          Secondary Button
+        </Button>
+      </Paper>
+    </Container>
   );
 }
-
 export default App;
