3d003c0cc788763b02a0e7fec2e060aad4b06f05
Paul Edwards <paul.edwards@microsoft.com>
2020-10-07 18:12:44 +0100

    Updated to create theme

---
+++ src/theme.js
@@ -0,0 +1,9 @@
+import { indigo, red } from '@material-ui/core/colors';
+import { createMuiTheme } from '@material-ui/core/styles';
+// A custom theme for this app
+const theme = createMuiTheme({
+  palette: {
+    type: 'dark'
+  }
+});
+export default theme;
