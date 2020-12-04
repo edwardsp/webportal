b76b842dc8900d9123bc4383723b84e4edf85b30
Paul Edwards <paul.edwards@microsoft.com>
2020-12-04 13:24:18 +0000

    Serve the build react app

--- api/api.py
+++ api/api.py
@@ -2,7 +2,11 @@ import json
 import time
 from flask import Flask, request
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='../build', static_url_path='/')
+
+@app.route('/')
+def index():
+    return app.send_static_file('index.html')
 
 state = {
     'users': [
